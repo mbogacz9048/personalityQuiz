38a7a97c66fd34cd796792767381a7928d9558e3
Matt Bogacz <mbogacz9048@jh-mba158-05.d214.org>
2018-03-06 11:34:43 -0600

    added 3 new scenes to view controller and button with progress bar as well

---
+++ personalityQuiz/ResultsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultsViewController.swift
+//  personalityQuiz
+//
+//  Created by Matt Bogacz on 3/5/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import Foundation
