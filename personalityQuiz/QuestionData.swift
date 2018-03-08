8c9da59971bae4c4be4c7e1c9d1ae5793c648769
Matt Bogacz <mbogacz9048@jh-mba158-05.d214.org>
2018-03-08 11:04:59 -0600

    Added QuestionsViewcontroller class

--- personalityQuiz/QuestionData.swift
+++ personalityQuiz/QuestionData.swift
@@ -22,3 +22,19 @@ struct Answer {
     var text : String
     var type: AnimalType
 }
+
+enum AnimalType: Character {
+    case dog = "🐶", rabbit = "🐰", turtle = "🐢", cat = "🐱"
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love, and enjoy activities with your friends"
+        case .cat:
+            return "Mischevious, yet mild-tempered, you enjoy doing things on your own terms."
+        case .rabbit:
+            return "You love everything that's soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
+}
